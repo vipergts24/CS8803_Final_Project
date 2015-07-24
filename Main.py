51ff668ae226999d548859bd51848601c3605773
Craig Abernethy <craigabernethy85@gmail.com>
2015-07-24 10:12:52 -0700

    Working EKF implemented

--- Main.py
+++ Main.py
@@ -41,14 +41,24 @@ def kalman_filter(measurement,OTHER,X,P):
     turningAngle = OTHER['turningAngle']
     predictedAngle = angle_trunc(angle+turningAngle)
     distance = OTHER['distance']
+    #print 'last X,Y: ', (lastX,lastY)
+    #print 'predictedAngle: ',predictedAngle
+    #print 'distance: ', distance
+    X.value[0][0] = measurement[0]
+    X.value[1][0] = measurement[1]
+    X.value[2][0] = angle
+    X.value[3][0] = turningAngle
+    X.value[4][0] = distance
     #Prediction
-    F = matrix([[1.,0.,0.,0.,-sin(predictedAngle)],
-               [0.,1.,0.,0.,cos(predictedAngle)],
-               [0.,0.,1.,0.,0.],
+    F = matrix([[1.,0.,0.,0.,cos(predictedAngle)],
+               [0.,1.,0.,0.,sin(predictedAngle)],
+               [0.,0.,1.,1.,0.],
                [0.,0.,0.,1.,0.],
                [0.,0.,0.,0.,1.]])
-    
-    X = (F * X) + u    
+    #print 'X: ', X
+    X = (F * X) + u
+    #print 'F: ', F
+    #print 'F*X = ', X
     P = F * P * F.transpose()
     
     #measurement update
@@ -56,7 +66,7 @@ def kalman_filter(measurement,OTHER,X,P):
     S = H * P * H.transpose() + R
     K = P * H.transpose() * S.inverse()
 
-    X = X + (K * Y)
+    newX = X + (K * Y)
     P = (I - (K * H)) * P
     
     return X,P
@@ -88,7 +98,7 @@ def readData(textFile):
             x = int(val[0])
             y = int(val[1])
             data.append((x,y))
-            print 'Measured: ',x, ',',y,'\n'
+            print 'Measured: ',x, ',',y,'\n'                
             broken_robot.goto(int(x)/2-500, 300-int(y)/2) # flip the y and offset the x
             broken_robot.pendown() # or .stamp()
             global X
@@ -97,9 +107,9 @@ def readData(textFile):
                 #Initial state {x,y,xVelocity,yVelocity}
                 X = matrix([[x],
                             [y],
-                            [0.],
-                            [0.],
-                            [0.]])
+                            [1.],
+                            [1.],
+                            [1.]])
                 #Initial Uncertainty
                 P = matrix([[1000.,0.,0.,0.,0.],
                             [0.,1000.,0.,0.,0.],
@@ -107,16 +117,23 @@ def readData(textFile):
                             [0.,0.,0.,1000.,0.],
                             [0.,0.,0.,0.,1000.]])
                 OTHER = {'lastMeasurement':(x,y)}
+            else:
+                #Calculates the difference between prediction and measurement
+                errorPercentX = float((x-X.value[0][0])/x)
+                errorPercentY = float((y-X.value[1][0])/y)
+                errorPercentage = (abs(errorPercentX) + abs(errorPercentY))*100
+                print 'Error Percentage: ', errorPercentage
             angle = atan2(y-OTHER['lastMeasurement'][1],x-OTHER['lastMeasurement'][0])
             angle = angle_trunc(angle)
-            if 'lastAngle' not in OTHER:
-                OTHER['lastAngle'] = angle
+            OTHER['lastAngle'] = angle
             OTHER['turningAngle'] = angle - OTHER['lastAngle']
             OTHER['distance'] = distance_between((x,y),OTHER['lastMeasurement'])
             X,P = kalman_filter((x,y),OTHER,X,P)
+            OTHER['lastMeasurement'] = (x,y)
             print 'Predicted: ',X,'\n'
             predict_robot.goto(int(X.value[0][0])/2-500, 300-int(X.value[1][0])/2) # flip the y and offset the x
             predict_robot.pendown() # or .stamp()
+
     #except:
     #    print 'Failed to read and process file: ',textFile, '\n'
     finally:
